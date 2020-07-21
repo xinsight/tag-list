bfbc7485cfd1d3cc13b57fac7460255b2b03134e
Jay Moore <jmoore@ehealthinnovation.org>
2020-07-21 14:51:58 +0200

    fix syntax

--- Package.swift
+++ Package.swift
@@ -20,8 +20,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "tag-list",
-            dependencies: []),
-            path: "Source",
+            dependencies: [],
+            path: "Source"),
         .testTarget(
             name: "tag-listTests",
             dependencies: ["tag-list"]),
