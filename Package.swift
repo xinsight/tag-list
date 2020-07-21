b0aa5fa32f14812cb415e0df1935549d938f0075
Jay Moore <jmoore@ehealthinnovation.org>
2020-07-21 15:15:34 +0200

    remove hyphen

--- Package.swift
+++ Package.swift
@@ -4,13 +4,13 @@
 import PackageDescription
 
 let package = Package(
-    name: "tag-list",
+    name: "TagsList",
     platforms: [.iOS(.v13)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "tag-list",
-            targets: ["tag-list"]),
+            name: "TagsList",
+            targets: ["TagsList"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -20,7 +20,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "tag-list",
+            name: "TagsList",
             dependencies: [],
             path: "Source"),
         //.testTarget(
